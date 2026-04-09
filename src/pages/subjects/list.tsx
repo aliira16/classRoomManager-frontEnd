1ed42559d32282cc442f1a013fdd9d9345768380
aliira16 <rouaneali27@gmail.com>
2026-04-09 20:49:19 +0100

    add refine/rest

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -58,7 +58,7 @@ const SubjectList = () => {
         },
         {
           id: "department",
-          accessorKey: "department",
+          accessorKey: "department.name",
           size: 150,
           header: () => <p className="Column-title">Department</p>,
           cell: ({ getValue }) => (
--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,34 +1,4 @@
-import {
-  BaseRecord,
-  DataProvider,
-  GetListParams,
-  GetListResponse,
-} from "@refinedev/core";
+import { DataProvider } from "@refinedev/core";
+import { simpleRestDataProvider } from "@refinedev/simple-rest";
 
-// mock-subjects.ts
-import { MOCK_SUBJECTS } from "@/constants/mock-data";
-
-export const dataProvider: DataProvider = {
-  getList: async <TData extends BaseRecord = BaseRecord>({
-    resource,
-  }: GetListParams): Promise<GetListResponse<TData>> => {
-    if (resource !== "subjects") return { data: [] as TData[], total: 0 };
-    return {
-      data: MOCK_SUBJECTS as unknown as TData[],
-      total: MOCK_SUBJECTS.length,
-    };
-  },
-  getOne: async () => {
-    throw new Error("This function is not present in Mock ");
-  },
-  create: async () => {
-    throw new Error("This function is not present in Mock ");
-  },
-  update: async () => {
-    throw new Error("This function is not present in Mock ");
-  },
-  deleteOne: async () => {
-    throw new Error("This function is not present in Mock ");
-  },
-  getApiUrl: () => "",
-};
+export const dataProvider: DataProvider = simpleRestDataProvider("http://localhost:8000/api");
